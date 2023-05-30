31eedcba0637fee68188b9a69c2e3a3ced8e19cf
Andrea Petrucci <andrea.petrucci@edu.hefr.ch>
2023-05-30 17:06:56 +0200

    added more size for post

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
